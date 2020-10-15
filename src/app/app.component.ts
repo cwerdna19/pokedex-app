1e6620903cc54d7daff7208332e7a9e015b67607
cwerdna19 <thequantum.andrew@gmail.com>
2020-10-15 16:38:00 -0500

    Test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'pkdx-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'pokedex-app';
+  pageTitle: string = 'Pokedex';
 }
